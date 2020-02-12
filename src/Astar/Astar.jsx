e854f4151dc85c0b5f690174e9e8dd8eeb5259f1
Moltenfuzzy <kentphan2000@gmail.com>
2020-02-12 09:18:47 -0800

    got mouse over to change background color of cell AND refactored Astar.jsx

--- src/Astar/Astar.jsx
+++ src/Astar/Astar.jsx
@@ -6,39 +6,44 @@ export default class Astar extends React.Component {
   
   constructor() {
     super();
-    this.grid = 0; // ??? init
+    this.state = {
+      grid: []
+    }
   }
 
-  create2DArray(numRows, numColumns) {
-    let array = new Array(numRows); 
-   
-    for(let i = 0; i < numColumns; i++) {
-      array[i] = new Array(numColumns); 
-    }
-   
-    return array; 
-  }
-
-  // fills grid with component
-  fillGrid(obj) {
-    let grid = this.create2DArray(20,20);
-    
-    // .length only works for n by n matrices
-    for(let i = 0; i < grid.length; i++) {
-      for(let j = 0; j < grid.length; j++) {
-        grid[i][j] = obj; 
-      }
-    }
-
-    return grid; 
+  componentDidMount() {
+    const grid = getInitialGrid();
+    this.setState({grid});
   }
 
   render() {
-    this.grid = this.fillGrid(<Cell />);
     return (
       <div className="grid">
-        {this.grid}
+        {this.state.grid}
       </div>
     );
   }
+}
+
+function getInitialGrid() {
+  let grid = create2DArray(20,20);
+
+  // .length only works for n by n matrices
+  for(let i = 0; i < grid.length; i++) {
+    for(let j = 0; j < grid.length; j++) {
+      grid[i][j] = <Cell row={i} col={j} />; 
+    }
+  }
+
+  return grid; 
+}
+
+function create2DArray(numRows, numColumns) {
+  let array = new Array(numRows); 
+ 
+  for(let i = 0; i < numColumns; i++) {
+    array[i] = new Array(numColumns); 
+  }
+ 
+  return array; 
 }
--- src/Astar/Cell/Cell.jsx
+++ src/Astar/Cell/Cell.jsx
@@ -3,21 +3,42 @@ import "./Cell.css";
 
 export default class Cell extends React.Component {
 
-	constructor() {
-		super(); 
+	constructor(props) {
+		super(props); 
 		this.state = { 
-				col: 0, 
-				row: 0,
-				color: "white",
-				isWall: false,
+				row: props.row,
+				col: props.col,
+				isWall: props.isWall,
+				isStart: props.isStart,
+				isEnd: props.isEnd
 		}
+		this.handleMouseEnter = this.handleMouseEnter.bind(this); 
+		this.handleMouseDown = this.handleMouseDown.bind(this); 
+	}
+
+	handleMouseEnter() {
+		this.setState({isWall: true});
+		console.log("works");
+	}
+
+	handleMouseDown() {
+		console.log("Down");
+		console.log(`Row: ${this.state.row}`);
+		console.log(`Col: ${this.state.col}`);
 	}
 
 	render() {
+		const extraClassName = 
+		this.state.isEnd ? 'cell-end' : 
+		this.state.isStart ? 'cell-start': 
+		this.state.isWall ? 'cell-wall' : '';
+
 		return (
 			<div 
-				className="cell"
+				className={`cell ${extraClassName}`}
 				id = {`cell ${this.state.row} ${this.state.col}`}
+				onMouseEnter={this.handleMouseEnter}
+				onMouseDown={this.handleMouseDown}
 			>
 			</div>
 		);
